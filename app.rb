0ac7c4bdb006a4e7b7ecbd69032e2b99628d08dc
harukasan <harukasan@pixiv.com>
2015-02-05 12:10:35 +0900

    Add supports to merge_request hook

--- app.rb
+++ app.rb
@@ -5,6 +5,12 @@ require 'slim'
 
 Idobata.hook_url = ENV['HOOK_URL']
 
+Gitlab.configure do |c|
+  c.endpoint = ENV['GITLAB_API_ENDPOINT']
+  c.private_token = ENV['GITLAB_API_PRIVATE_TOKEN']
+end
+
+
 class App < Sinatra::Base
   post '/push.json' do
     data = JSON.parse request.body.read
@@ -15,6 +21,21 @@ class App < Sinatra::Base
         Idobata::Message.create source: message, format: :html if message
       end
     end
+
+    { status: "ok" }.to_json
+  end
+
+  post '/merge_request.json' do
+    data = JSON.parse request.body.read
+    project = Gitlab.project(data["object_attributes"]["target_project_id"]).to_hash
+    mr = Gitlab.merge_request(data["object_attributes"]["target_project_id"], data["object_attributes"]["id"]).to_hash
+    p project
+    p data
+    p mr
+
+    message = Slim::Template.new("templates/merge_request.slim").render(self, data: data, project: project, mr: mr)
+    p message
+    Idobata::Message.create source: message, format: :html if message
 
     { status: "ok" }.to_json
   end
